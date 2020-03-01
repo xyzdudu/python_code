af524a32f33f962b4c214ff6a475b19f228f18c7
xyzdudu <68288504@qq.com>
2020-03-01 17:32:55 +0800

    master

--- L7.py
+++ L7.py
@@ -1,24 +1,40 @@
-# 引入requests
-import requests, bs4
-# 封装headers
+import requests, csv
 headers={'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'}
-# 写入网址
 url='https://www.zhihu.com/api/v4/members/zhang-jia-wei/articles?'
-# 封装参数
-params={
-    'include':'data[*].comment_count,suggest_edit,is_normal,thumbnail_extra_info,thumbnail,can_comment,comment_permission,admin_closed_comment,content,voteup_count,created,updated,upvoted_followees,voting,review_info,is_labeled,label_info;data[*].author.badge[?(type=best_answerer)].topics',
-    'offset':'10',
-    'limit':'10',
-    'sort_by':'create',
-    }
-# 发送请求，并把响应内容赋值到变量res里面
-res=requests.get(url,headers=headers,params=params)
-# 确认请求成功
-print(res.status_code)
-# 用json()方法解析response对象，并赋值给变量articles
-# articles=res.json()
-bs = bs4.BeautifulSoup(res.text, 'html.parser')
-items = bs.find(class_='ListShortcut').find(class_='List').find_all(class_='List-item')
-for item in items:
-    name = item.find('h2').text
-    print(name)
+articlelist=[]
+#建立一个空列表，以待写入数据
+offset=0
+#设置offset的起始值为0
+
+csv_file=open('L7.csv','w',newline='')
+writer = csv.writer(csv_file)
+while True:
+    params={
+        'include':'data[*].comment_count,suggest_edit,is_normal,thumbnail_extra_info,thumbnail,can_comment,comment_permission,admin_closed_comment,content,voteup_count,created,updated,upvoted_followees,voting,review_info,is_labeled,label_info;data[*].author.badge[?(type=best_answerer)].topics',
+        'offset':str(offset),
+        'limit':'20',
+        'sort_by':'voteups',
+        }
+    #封装参数
+    res=requests.get(url,headers=headers,params=params)
+    #发送请求，并把响应内容赋值到变量res里面
+    articles=res.json()
+    # print(articles)
+    data=articles['data']
+    #定位数据
+    for i in data:
+        list1=[i['title'],i['url'],i['excerpt']]
+        #把数据封装成列表
+        articlelist.append(list1) 
+        writer.writerow(list1)
+    offset=offset+20
+    #在while循环内部，offset的值每次增加20
+    if offset>40:
+        break
+    #如果offset大于40，即爬了两页，就停止
+    #if articles['paging']['is_end'] == True:
+    #如果键is_end所对应的值是True，就结束while循环。
+        #break
+csv_file.close()
+print('okay') 
+#打印看看
